68a4f248c83d599e28fd7129c0a242c12a8543e8
Claire <clairefraser121@gmail.com>
2024-01-13 01:51:24 +0000

    added Project.jsx

---
+++ src/components/Project/index.jsx
@@ -0,0 +1,16 @@
+// src/components/Project.jsx
+import React from 'react';
+
+const Project = ({ title, description, imageUrl, deployedLink, githubLink }) => {
+  return (
+    <div>
+      <h3>{title}</h3>
+      <p>Description: {description}</p>
+      <img src={imageUrl} alt={title} />
+      <p>Link to deployed version: <a href={deployedLink}>{deployedLink}</a></p>
+      <p>Link to GitHub repository: <a href={githubLink}>{githubLink}</a></p>
+    </div>
+  );
+}
+
+export default Project;
