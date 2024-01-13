537906053b487294d2ea7ed9cc7f4d15fa8d526b
Claire <clairefraser121@gmail.com>
2024-01-13 02:08:08 +0000

    added id to ProjectCard

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -1,15 +1,17 @@
 // src/components/ProjectCard.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
-import "./style.css";
+import "../ProjectCard/style.css";
 
-const ProjectCard = ({ title, description, imageUrl, projectId }) => {
+const ProjectCard = ({ id, title, description, imageUrl }) => {
   return (
-    <Link to={`/project/${projectId}`} className="project-card">
-      <img src={imageUrl} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </Link>
+    <div className="project-card">
+      <Link to={`/project/${id}`}>
+        <img src={imageUrl} alt={title} />
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </Link>
+    </div>
   );
 }
 
