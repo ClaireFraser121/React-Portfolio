8009e8f030d07046c6bc553e8e035fba8316947d
Claire <clairefraser121@gmail.com>
2024-01-13 01:32:01 +0000

    udpated imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,46 @@
-// src/App.jsx
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Container, Navbar, Nav, Button } from 'react-bootstrap'; // Importing Bootstrap components from react-bootstrap
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+import NavPage from './components/Nav';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
-import ProjectDetails from './pages/ProjectDetail'; // Make sure this path is correct
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-
+import ProjectDetails from './pages/ProjectDetail';
+import ContactForm from './components/ContactForm';
+import './styles/main.css'; // Import your custom styles
+import './App.css';  // Custom styles for App component
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Router>
-      <>
+      <div id="wrapper">
         <Navbar bg="dark" variant="dark">
           <Container>
-            <Navbar.Brand href="#home">React Bootstrap App</Navbar.Brand>
+            <Navbar.Brand href="/">Claire's Portfolio</Navbar.Brand>
             <Nav className="me-auto">
               <Nav.Link href="/">Home</Nav.Link>
               <Nav.Link href="/projects">Projects</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
             </Nav>
-            <Button variant="outline-light" onClick={() => setCount((prevCount) => prevCount + 1)}>
-              Increment Count
-            </Button>
           </Container>
         </Navbar>
 
         <Container className="mt-3">
           <Routes>
-            <Route path="/" element={<Home count={count} setCount={setCount} />} />
+            <Route path="/" element={<Home />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/project/:projectId" element={<ProjectDetails />} />
+            <Route path="/header" element={<Header />} />
+            <Route path="/nav" element={<NavPage />} />
+            <Route path="/contact" element={<ContactForm />} />
           </Routes>
         </Container>
 
         <Footer />
-      </>
+      </div>
     </Router>
   );
 }
